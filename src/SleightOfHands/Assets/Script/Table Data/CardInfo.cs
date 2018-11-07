9931c52ae6a1fc89d98db7ac3fade624d44386a8
Zheyu Zhang <eric.zhangzheyu@gmail.com>
2018-11-06 23:49:25 -0500

    Corrected loading CardInfo.cs

--- src/SleightOfHands/Assets/Script/Table Data/CardInfo.cs
+++ src/SleightOfHands/Assets/Script/Table Data/CardInfo.cs
@@ -2,18 +2,23 @@
 {
     public int Id { get; private set; }
     public string Name { get; private set; }
-    public string Description { get; private set; }
     public string Template { get; private set; }
     public string Illustration { get; private set; }
+    public string Description { get; private set; }
 
     int ITableDataEntry.Set(int row, string[] stringData)
     {
         Id = int.Parse(stringData[0]);
         Name = stringData[1];
-        Description = stringData[2];
-        Template = stringData[3];
-        Illustration = stringData[4];
+        Template = stringData[2];
+        Illustration = stringData[3];
+        Description = stringData[4];
 
         return Id;
+    }
+
+    public override string ToString()
+    {
+        return string.Format("[{0}] {1}: {2} (template: {3}; illustration: {4})", Id, Name, Description, Template, Illustration);
     }
 }
